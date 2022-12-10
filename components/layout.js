b0cef12e3a1dfd763162a50c817813697c3b16d5
Hongorzul1 <soyombokhongorzul@gmail.com>
2022-12-10 10:00:16 +0800

    aldaa zassan

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Xngro'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
